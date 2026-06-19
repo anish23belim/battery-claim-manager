e7a780af4cb64f6c6f014c4d5c9a1f19a34a4fc0
anish23belim <anish23belim@gmail.com>
2026-06-19 23:57:10 +0530

    Fix typescript error in prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("../../node_modules/.prisma/client");
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
